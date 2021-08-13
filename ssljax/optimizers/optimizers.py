4fe4ea54cef349bcaf43211f7315206165bfcd4b
ryanccarelli <ryancarelli@gmail.com>
2021-08-12 23:37:30 -0400

    register optimizers

--- ssljax/optimizers/optimizers.py
+++ ssljax/optimizers/optimizers.py
@@ -1,8 +1,23 @@
-from optax import adagrad, adam, adamw, lamb, rmsprop
+from optax import (adabelief, adagrad, adam, adamw, dpsgd, fromage, lamb,
+                   noisy_sgd, radam, rmsprop, sgd, yogi)
+from ssljax.core.utils import Registrable
 
 
-@Optimizer.register("adagrad")(adagrad)
-@Optimizer.register("adam")(adam)
-@Optimizer.register("adamw")(adamw)
-@Optimizer.register("lamb")(lamb)
-@Optimizer.register("rmsprop")(rmsprop)
+class Optimizer(Registrable):
+    pass
+
+
+Registrable._registry[Optimizer] = {
+    "adabelief": adabelief,
+    "adagrad": adagrad,
+    "adam": adam,
+    "adamw": adamw,
+    "dpsgd": dpsgd,
+    "fromage": fromage,
+    "lamb": lamb,
+    "noisy_sgd": noisy_sgd,
+    "radam": radam,
+    "rmsprop": rmsprop,
+    "sgd": sgd,
+    "yogi": yogi,
+}
