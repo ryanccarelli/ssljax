4d25139a7434fc6ee0e16c4f3e27bf522dc9927a
ryanccarelli <ryancarelli@gmail.com>
2021-08-13 20:01:01 -0400

    isolate jax installation

--- setup.py
+++ setup.py
@@ -16,7 +16,8 @@ setuptools.setup(
         "Documentation": "https://ssljax.readthedocs.io/en/stable",
         "Source Code": "https://github.com/Dana-Farber-AIOS/pathml",
     },
-    install_requires=["numpy", "pre-commit==2.13.0", "torch", "jax", "flax",],
+    install_requires=["numpy", "pre-commit==2.13.0", "torch", "flax",],
+    extras_require=["jax>=0.1.71", "jaxlib>=0.1.49",],
     classifiers=[
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Programming Language :: Python :: 3",
