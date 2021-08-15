56fcd308471d8c7ad23bd7bb786d3e38315534e4
ryanccarelli <ryancarelli@gmail.com>
2021-08-15 16:18:44 -0400

    add head and body base classes

---
+++ ssljax/models/body/body.py
@@ -0,0 +1,5 @@
+from ssljax.core.utils import Registrable
+from flax import linen as nn
+
+class Body(Registrable, nn.Module):
+    pass
---
+++ ssljax/models/head/head.py
@@ -0,0 +1,5 @@
+from ssljax.core.utils import Registrable
+import flax.linen as nn
+
+class Head(Registrable, nn.Module):
+    pass
