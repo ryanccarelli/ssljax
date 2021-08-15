f676bc672d37d3efb1011d3290fa81fec1f939d7
ryanccarelli <ryancarelli@gmail.com>
2021-08-15 16:25:14 -0400

    finished base skeleton

--- ssljax/models/base.py
+++ ssljax/models/base.py
@@ -19,30 +19,18 @@ from ssljax.config import FromParams
 
 
 class BaseSSL(FromParams, nn.Module):
-import jax
-import jax.numpy as jnp
-from flax import linen as nn
-from ssljax.config import FromParams
-
-
-class BaseSSL(FromParams, nn.Module):
     """
     Base class implementing self-supervised model.
 
     Args:
         config (json/yaml?): model specification
-        params (str): path to model parameters
     """
 
-    def __init__(self, config, params: None):
+    def __init__(self, config):
         self.config = config
         self.params = params
-        # TODO: read config
-        self.head = self._setup_head(config)
-        self.body = self._setup_body(config)
-        self.loss = self._setup_loss(config)
-        self.optimizer = self._setup_optimizer(config)
-        self.trainer = self._setup_trainer(config)
+        self.head = Head.from_params(self.config.pop("head"))
+        self.body = Body.from_params(config.pop("body"))
 
     def __call__(self, x):
         """
@@ -54,23 +42,6 @@ class BaseSSL(FromParams, nn.Module):
     def from_params():
         raise NotImplementedError
 
-    def _setup_head():
-        """
-        Read from config and instantiate head
-
-        Returns:
-            list
-        """
-        raise NotImplementedError
-
-    def _setup_body():
-        # read from config and instantiate body
-        raise NotImplementedError
-
-    def _setup_loss():
-        # read from config and instantiate loss
-        raise NotImplementedError
-
     def freeze_head():
         raise NotImplementedError
 
@@ -78,8 +49,4 @@ class BaseSSL(FromParams, nn.Module):
         raise NotImplementedError
 
     def is_frozen():
-        raise NotImplementedError
-
-    @jax.jit
-    def apply_model():
         raise NotImplementedError
