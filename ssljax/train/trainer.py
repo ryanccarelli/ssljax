0d806c7fb0aa4f895bcc5566d75c08aae0e0213a
ryanccarelli <ryancarelli@gmail.com>
2021-08-17 16:58:28 -0400

    task and trainer redesign

--- ssljax/train/trainer.py
+++ ssljax/train/trainer.py
@@ -15,43 +15,43 @@ class SSLTrainer:
     """
     Class to manage model training and feature extraction.
 
-    Prepares experiment from config file:
-        - optimizer
-        - loss
-        - model
-
     Args:
         rng (jnp.DeviceArray):
-        config (): configuration file
+        config (json): configuration file
     """
     def __init__(self, rng, config):
         self.config = config
         self.rng = prepare_environment(self.config)
         self.task = self.build_task(config)
-        self.model = task.model
 
     def train(self):
-        # recover training state from checkpoint
-        # TODO: parse config
+        # setup devices
         platform = jax.local_devices()[0].platform
         dynamic_scale = None
         if self.config.pop("half_precision") and platform == "gpu":
             dynamic_scale = optim.DynamicScale()
-        _ = self._load_training_state(self.config.pop(""))
-        # TODO: should be shape of input data
-        init_data = jnp.ones((task.batch_size, TODO), jnp.float32)
-        state = TrainState.create(
-            apply_fn=self.model.apply,
-            params=self.model.init(key, init_data, self.rng)["params"],
-            tx=opt,
-        )
-        # TODO: make this work
-        for data in iter(task.dataset)
-            batch = jax.device_put(data)
-            task.augment(batch)
-            self.epoch(batch, )
+        # instantiate training state
+        if self.config.pop("load_training_state"):
+            state = self._load_training_state(self.config.pop(""))
+        else:
+            # TODO: should be shape of input data
+            init_data = jnp.ones((task.batch_size, TODO), jnp.float32)
+            state = TrainState.create(
+                apply_fn=self.task.model.apply,
+                params=self.task.model.init(key, init_data, self.rng)["params"],
+                tx=opt,
+            )
+        # get parameters
+        lr = task.scheduler.lr(state.step)
+        # TODO: If loss returns auxiliary data, pass has_aux=True
+        grad_fn = jax.value_and_grad(task.loss)
+        for data, targets in iter(task.dataset)
+            train_data = jax.device_put(data)
+            targets = jax.device_put(targets)
+            task.augment(train_data)
+            state = self.epoch(train_data, targets, grad_fn, state)
 
-    def epoch(self, train_data, batch_size, rng):
+    def epoch(self, train_data, targets, grad_fn, state, batch_size, rng):
         # get trainingset size
         train_data_size = len(train_data)
         steps_per_epoch = train_data_size // batch_size
@@ -62,47 +62,35 @@ class SSLTrainer:
         batch_metrics = []
         for perm in perms:
             batch = {k: v[perm, ...] for k, v in train_data.items()}
-            state, metrics = self.step(batch)
+            state, metrics = self.step(batch, targets, grad_fn, state)
             batch_metrics.append(metrics)
         # compute mean of metrics across each batch in epoch.
         # log metrics
         batch_metrics_np = jax.device_get(batch_metrics)
         epoch_metrics_np = {
             k: np.mean([metrics[k] for metrics in batch_metrics_np])
-            for k in batch_metrics_np[0]}
-        print('train epoch: %d, loss: %.4f, accuracy: %.2f' % (
-            epoch, epoch_metrics_np['loss'], epoch_metrics_np['accuracy'] * 100))
+            for k in batch_metrics_np[0]
+        }
+        # TODO: log
+        logger.log(f"train epoch: {epoch}, loss: {epoch_metrics_np['loss']}, accuracy: {epoch_metrics_np['accuracy']}")
         return state
 
     @jax.jit
-    def step(self, batch, state):
+    def step(self, batch, targets, state):
         """
         Compute gradients, loss, accuracy per batch
         """
-        # get loss function
-        # forward pass
-        grad_fn = jax.value_and_grad(task.loss, has_aux=True)
         (loss, logits), grads = grad_fn(self.state.params)
+        grads = jax.lax.pmean(grads, "batch")
         state = state.apply_gradients(grads=grads)
-        # TODO: dynamic scale?
-        metrics = # compute metrics here
+        metrics = self.task.metrics(logits, targets, weights)
+        # summary = jax.tre_map(lambda x: x.mean(), train_metrics)
         return state, metrics
 
-    @property
-    def model():
-        # return subclass of BaseSSL from config
-        return self._model
-
-    @setter
-    def model(config):
-        # init model from config
+    def eval():
         raise NotImplementedError
 
-    def optimizer(config):
-        # return optimizer from config
-        raise NotImplementedError
-
-    def _load_training_state(self, config):
+    def evalstep():
         raise NotImplementedError
 
     def build_task(self, config):
