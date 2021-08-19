56e7b8e8523abac9f3c3eb8894b47aa54cb48c62
ryanccarelli <ryancarelli@gmail.com>
2021-08-18 21:44:11 -0400

    implement augmentation and pipeline classes

--- ssljax/augment/base.py
+++ ssljax/augment/base.py
@@ -1,2 +1,58 @@
 # base class for augmentations here
 # augmentations will be used by the trainer
+import jax.numpy as jnp
+
+
+class Augmentation:
+    """
+    An augmentation is a function applied to images
+    """
+
+    def __call__(self, x):
+        raise NotImplementedError
+
+
+class Pipeline(Augmentation):
+    """
+    Compose a sequence of augmentations.
+
+    Args:
+        augmentations (list): sequence of transforms to be consecutively applied.
+            List of `pathml.core.Transform` objects
+    """
+
+    def __init__(self, augmentlist):
+        assert all([isinstance(t, Augmentation) for t in augmentlist]), (
+            f"All elements in input list must be of"
+            f" type ssljax.augment.Augmentation"
+        )
+        self.pipeline = augmentlist
+
+    def __len__(self):
+        return len(self.pipeline)
+
+    def __repr__(self):
+        out = f"Pipeline([\n"
+        for t in self.pipeline:
+            out += f"\t{repr(t)},\n"
+        out += "])"
+        return out
+
+    def __call__(self, x):
+        # this function has side effects
+        # modifies the tile in place, but also returns the modified tile
+        # need to do this for dask distributed
+        assert isinstance(x, jnp.array), f"argument of type {type(x)} must be __."
+        for t in self.pipeline:
+            aug = t()
+            x = aug(x)
+        return x
+
+    def save(self, path):
+        """
+        save pipeline to disk
+
+        Args:
+            path (str): save path on disk
+        """
+        raise NotImplementedError
