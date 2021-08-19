25e2dbd30ca2749cc2381c636764357f6d5f6779
Gabe <gabeorlanski@gmail.com>
2021-08-19 12:56:53 -0400

    Added Docs

---
+++ ssljax/config/config.py
@@ -0,0 +1,8 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+logger.error(f"{__name__}: THESE ARE PLACEHOLDERS!!")
+
+class Config:
+    pass
--- ssljax/tasks/task.py
+++ ssljax/tasks/task.py
@@ -3,11 +3,10 @@ import logging
 
 # TODO(gabeorlanski): Change these to reflect init files
 from ssljax.augment.base import AugmentBase
-# from ssljax.config import FromParams
-from ssljax.core import register, FromParams
-from ssljax.data import DataloaderBase, DatasetBase
+from ssljax.config import Config
+from ssljax.data import DataloaderBase
 from ssljax.losses import LossBase
-from ssljax.models import ModelBase, SSLBase
+from ssljax.models import ModelBase
 from ssljax.optimizers import OptimizerBase
 from ssljax.train import MeterBase, SchedulerBase
 
@@ -28,43 +27,82 @@ class TaskBase:
         - augmentations
         - scheduler
             - lr
+
+    Args:
+        config (Config): The config to get parameters from.
+
+
     """
 
-    def __init__(self, config):
+    def __init__(self, config:Config):
+
         super().__init__()
         self.config = config
         self.model = self._get_model()
         self.loss = self._get_loss()
         self.optimizer = self._get_optimizer()
-        self.dataset = self._get_dataset()
         self.dataloader = self._get_dataloader()
         self.meter = self._get_meter()
         self.augment = self._get_augment()
 
+    #####################################################################
+    # Functions the children class must implement                       #
+    #####################################################################
     def _get_optimizer(self) -> OptimizerBase:
         """
+        Initialize the optimizer. This must be implemented by child tasks.
 
-        :return:
+        Returns (OptimizerBase): The optimizer to use for the task.
         """
         raise NotImplementedError()
 
-    # def _get_dataset(self) -> DatasetBase:
-    #     raise NotImplementedError()
-
     def _get_dataloader(self) -> DataloaderBase:
+        """
+        Initialize the dataloader. This must be implemented by child tasks.
+
+        Returns (DataloaderBase): The dataloader to use for the task.
+        """
         raise NotImplementedError()
 
     def _get_scheduler(self) -> SchedulerBase:
+        """
+        Initialize the scheduler. This must be implemented by child tasks.
+
+        Returns (SchedulerBase): The scheduler to use for the task.
+        """
         raise NotImplementedError()
 
     def _get_loss(self) -> LossBase:
+        """
+        Initialize the loss calculator. This must be implemented by child tasks.
+
+        Returns (LossBase): The loss to use for the task.
+        """
         raise NotImplementedError()
 
     def _get_meter(self) -> MeterBase:
+        """
+        Initialize the meter. This must be implemented by child tasks.
+
+        Returns (MeterBase): The meter to use for the task.
+        """
         raise NotImplementedError()
 
     def _get_model(self) -> ModelBase:
+        """
+        Initialize the model for this task. This must be implemented by child
+        tasks.
+
+        Returns (ModelBase): The model to use for this task.
+        """
         raise NotImplementedError()
 
     def _get_augment(self) -> AugmentBase:
+        """
+       Initialize the augment for this task. This must be implemented by child
+       tasks.
+
+       Returns (AugmentBase): The augment to use for this task.
+       """
         raise NotImplementedError()
+    
