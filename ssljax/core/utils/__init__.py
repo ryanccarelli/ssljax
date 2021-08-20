eaec8a2a7497d40df0ac6cbeb09577634578143b
arun <arun.anthony.kumar@gmail.com>
2021-08-20 01:06:03 +0100

    Remove useless import

--- ssljax/core/utils/__init__.py
+++ ssljax/core/utils/__init__.py
@@ -1,4 +1,3 @@
 """Utils is taken from allenNLP : https://github.com/allenai/allennlp"""
 from ssljax.core.utils.utils import *
 from ssljax.core.utils.register import register, get_from_register
-from ssljax.core.utils.from_params import FromParams
