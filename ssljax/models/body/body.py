bd8d9ab2db9b591fafc23671c254e7b193c232ab
ryanccarelli <ryancarelli@gmail.com>
2021-08-21 16:29:21 -0400

    remove head and body subclasses

--- ssljax/models/body/body.py
+++
@@ -1,5 +0,0 @@
-from ssljax.core.utils import Registrable
-from flax import linen as nn
-
-class Body(Registrable, nn.Module):
-    pass
--- ssljax/models/head/head.py
+++
@@ -1,5 +0,0 @@
-from ssljax.core.utils import Registrable
-import flax.linen as nn
-
-class Head(Registrable, nn.Module):
-    pass
