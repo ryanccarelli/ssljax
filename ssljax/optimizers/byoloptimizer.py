731338aeefbb90a931de8038735e473e115851fd
ryanccarelli <ryancarelli@gmail.com>
2021-08-26 13:28:50 -0400

    implement byol optimizer

--- ssljax/optimizers/byoloptimizer.py
+++ ssljax/optimizers/byoloptimizer.py
@@ -1,8 +1,15 @@
+from typing import Any, Optional
+
+import jax
+import jax.numpy as jnp
 import optax
+from optax._src import base
+from optax._src import utils as outils
+from optax._src.transform import EmaState, _bias_correction, _update_moment
 from ssljax.optimizers import lars
 
 
-def byoloptimizer(learning_rate, tau_learning_rate):
+def byol_optimizer(learning_rate, decay_rate):
     """
     Optimizer that applies LARS to online network and
     updates target network by exponential moving average.
@@ -10,35 +17,83 @@ def byoloptimizer(learning_rate, tau_learning_rate):
     Assumes that parameters are partitioned into
     "online" and "target" groups.
     Args:
-        learning_rate:
-        tau_learning_rate:
-            TODO: tau must be managed by its own cosine scheduler
+        learning_rate: for lars
+        decay rate: for ema
 
-    """
-    # parameters are partitioned in SSLModel through the Branch class
-    param_labels = ("target", "online")
-    return optax.multi_transform({"target": ema, "online": lars}, param_labels)
+    TODO: There are potentially cleaner implementations.
+    1. Use combine.chain() to execute lars (while masking byol_ema) then byol_ema
+    current version has EMA trailing by one step. Sketch of improved implementation:
 
-def byol_ema(tau) -> base.GradientTransformation:
+    optax.chain(
+        [
+            optax.multi_transform({"branch_0": identity, "branch_1":lars}),
+            optax.multi_transform({"branch_0": byol_ema, "branch_1": identity}),
+        ]
+    )
+
+    2. Use masking. The problem here is that we do not have access to state at this level:
+
+    mask = jax.tree_map(lambda x: x in treedef_children(state.params["branch_0"])))
+    optax.chain(
+        [optax.masked(lars, mask), optax.masked(byol_ema, !mask)]
+    )
     """
-    Exponential moving average with cosine decay as implemented
-    in Bootstrap Your Own Latent.
+    param_labels = ("branch_0", "branch_1")
+    return optax.multi_transform(
+        {"branch_0": byol_ema(decay_rate), "branch_1": lars(learning_rate)},
+        param_labels,
+    )
+
+
+def byol_ema(
+    decay: float, debias: bool = True, accumulator_dtype: Optional[Any] = None
+) -> base.GradientTransformation:
     """
+    Update target network as an exponential moving average of the online network.
+
+    Args:
+        decay: the decay rate for the exponential moving average.
+        debias: whether to debias the transformed gradient.
+        accumulator_dtype: optional `dtype` to used for the accumulator; if `None`
+            then the `dtype` is inferred from `params` and `updates`.
+    Returns:
+        An (init_fn, update_fn) tuple.
+    """
+
     def init_fn(params):
-        pass
+        return EmaState(
+            count=jnp.zeros([], jnp.int32),
+            ema=jax.tree_map(
+                lambda t: jnp.zeros_like(t, dtype=accumulator_dtype), params
+            ),
+        )
 
     def update_fn(updates, state, params=None):
-        # from state.params (flax.core.frozen_dict.FrozenDict) we get a dict
-        # and this dict needs to distinguish the target and online params
-        target_params = state.params[]
-        online_params = state.params[]
-        # optax.incremental_update
-        return updates, state
+        del params
+        # branch_0 is online, branch_1 is target
+        _update_moment(state.params["branch_0"], state.ema, decay, order=1)
+        count_inc = outils.safe_int32_increment(state.count)
+        if debias:
+            new_ema = _bias_correction(new_ema, decay, count_inc)
+        state_ema = outils.cast_tree(new_ema, accumulator_dtype)
+
+        return new_ema, EmaState(count=count_inc, ema=state_ema)
 
     return base.GradientTransformation(init_fn, update_fn)
 
-def _cosine_decay(global_step: jnp.ndarray, max_steps: int, initial_value: float) -> jnp.ndarray:
+
+def _update_moment(updates, moments, decay, order):
+    """Compute the exponential moving average of the `order-th` moment."""
+    return jax.tree_multimap(
+        lambda g, t: (1 - decay) * (g ** order) + decay * t, updates, moments
+    )
+
+
+def _cosine_decay(
+    global_step: jnp.ndarray, max_steps: int, initial_value: float
+) -> jnp.ndarray:
     """
+    Taken from 
     Simple implementation of cosine decay from TF1.
     This is used in BYOL optimizer to manage tau parameter.
     """
