70137b19829d0af2e0584580b70dffce8d2e0172
ryanccarelli <ryancarelli@gmail.com>
2021-08-26 14:41:27 -0400

    import byoloptimizer

--- ssljax/optimizers/__init__.py
+++ ssljax/optimizers/__init__.py
@@ -1,1 +1,3 @@
-from ssljax.optimizers.optimizers import Optimizer
+from .optimizers import (Optimizer, adabelief, adagrad, adam, adamw, dpsgd,
+                         fromage, lamb, noisy_sgd, radam, rmsprop, sgd, yogi)
+from .byoloptimizer import byol_optimizer 
