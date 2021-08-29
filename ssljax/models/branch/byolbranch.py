7050513c991daed29fdbd95abf2680c552f81496
ryanccarelli <ryancarelli@gmail.com>
2021-08-29 18:55:33 -0400

    implement byol branches

--- ssljax/models/branch/byolbranch.py
+++ ssljax/models/branch/byolbranch.py
@@ -1,5 +1,6 @@
 # branches for byol
 from ssljax.models.branch import Branch
+from ssljax.models.resnet import Resnet50
 
 
 class ByolOnlineBranch(Branch):
@@ -7,19 +8,26 @@ class ByolOnlineBranch(Branch):
     The online branch in BYOL model.
     Rep: Resnet50
     Proj: MLP
-    Pred: MLP
     """
 
-    def __call__(self):
-        pass
+    def setup(self):
+        self.resnet50 = Resnet50
+        self.mlp = MLP(layer_dims=[200, 10])
+
+    def __call__(self, x):
+        x = self.resnet50(x)
+        x = self.mlp(x)
+        return x
 
 
 class ByolTargetBranch(Branch):
     """
     The target branch in BYOL model.
     Rep: Resnet50
-    Proj: MLP
     """
+    def setup(self):
+        self.resnet50 = Resnet50
 
     def __call__(self):
-        pass
+        x = self.resnet50(x)
+        return x
