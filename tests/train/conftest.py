f523f22a1abcd57234ef79a27a3b7c3309a91485
ryanccarelli <ryancarelli@gmail.com>
2021-09-06 20:31:56 -0400

    minimal task branches

--- tests/train/conftest.py
+++ tests/train/conftest.py
@@ -1,12 +1,34 @@
+import flax.linen as nn
 import pytest
 from hydra import compose, initialize
+from ssljax.models.branch import Branch
 
 
 @pytest.fixture
-def nullconfig():
-    initialize(config_path="../train/conf/null_conf"):
+def cputestconfig():
+    initialize(config_path="../train/conf/null_conf")
     cfg = compose(config_name="config")
     return cfg
 
-class CPUOnlineBranch:
-    pass
+
+@register(Branch, "CPUOnlineBranch")
+class CPUOnlineBranch(Branch):
+    def setup(self):
+        self.linear = nn.Dense(1)
+
+    @nn.compact
+    def __call__(self, x):
+        x = self.linear(x)
+        x = self.linear(x)
+        return x
+
+
+@register(Branch, "CPUTargetBranch")
+class CPUTargetBranch(Branch):
+    def setup(self):
+        self.linear = nn.Dense(1)
+
+    @nn.compact
+    def __call__(self, x):
+        x = self.linear(x)
+        return x
