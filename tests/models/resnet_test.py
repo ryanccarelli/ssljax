9f6fa96c5e62969ff8fd307f83c8aebb0d16038a
Ryan Carelli <ryancarelli@gmail.com>
2021-11-30 13:53:46 -0500

    working resnet tests

--- tests/models/resnet_test.py
+++ tests/models/resnet_test.py
@@ -1,11 +1,18 @@
 # test ssljax/models/resnet.py
+from typing import Any
 
 import jax.numpy as jnp
 import jax.random
 import pytest
 from chex import assert_rank
 from ssljax.models.resnet import ResNet
+import optax
+from flax.training import train_state
+import flax.optim
 
+class TrainState(train_state.TrainState):
+    batch_stats: Any
+    dynamic_scale: flax.optim.DynamicScale
 
 # popular named resnet configurations
 @pytest.mark.parametrize(
@@ -32,7 +39,19 @@ class TestResnet:
         key = jax.random.PRNGKey(0)
         k1, _ = jax.random.split(key)
         x = jnp.ones((1, 224, 224, 3), jnp.float16)
-        params = resnet.init(k1, x, mutable="batch_stats")
-        out = resnet.apply(params, x)
+        params = resnet.init(k1, x)
+        tx = optax.sgd(
+            learning_rate=0.1,
+            momentum=0.1,
+            nesterov=True,
+        )
+        state = TrainState.create(
+            apply_fn=resnet.apply,
+            params=params["params"],
+            tx=tx,
+            batch_stats=params["batch_stats"],
+            dynamic_scale=None,
+        )
+        out, state = state.apply_fn({"params": params["params"], "batch_stats": state.batch_stats}, x, mutable=["batch_stats"])
         # assertions
-        assert_rank(out, 1)
+        assert len(out.shape) == 2
