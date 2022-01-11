5858231d8cb06eedd94de6e62b44e89909d3b712
Ryan Carelli <ryancarelli@gmail.com>
2022-01-10 22:04:11 -0500

    assert task.model is instance of sslmodel

--- tests/train/task_test.py
+++ tests/train/task_test.py
@@ -11,5 +11,5 @@ from ssljax.train.task import Task
 def test_init(basecpuconfig):
     task = Task(basecpuconfig)
     assert isinstance(task.trainer, SSLTrainer)
-    assert issubclass(task.model, SSLModel)
+    assert isinstance(task.model, SSLModel)
     assert issubclass(task.meter, SSLMeter)
