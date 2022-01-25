14323d967515ebb5c691037ca6e8b28dc1ce404e
Ryan Carelli <ryancarelli@gmail.com>
2022-01-25 15:06:42 -0500

    pass bit tests, add util functions to random resized crop

--- ssljax/data/dataloader.py
+++ ssljax/data/dataloader.py
@@ -7,7 +7,7 @@ import jax.numpy as jnp
 import jax.random
 import numpy as np
 import omegaconf
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 from scenic.dataset_lib.big_transfer import utils
 from scenic.dataset_lib.big_transfer.registry import Registry as ScenicRegistry
 from scenic.dataset_lib.datasets import get_dataset
@@ -103,41 +103,41 @@ def scenic(
     )
     return dataset
 
-
-@ScenicRegistry.register("identity", "function")
+@ScenicRegistry.register("preprocess_ops.random_resized_crops", "function")
 @utils.InKeyOutKey()
 @utils.BatchedImagePreprocessing()
-def get_identity():
-    """Passthrough function for scenic bit preprocessing"""
-
-    def _identity(image):
-        return image
-
-    return _identity
-
-
-@ScenicRegistry.register("random_resized_crop", "function")
-@utils.InKeyOutKey()
-@utils.BatchedImagePreprocessing()
-def get_random_resized_crop(
-    height: float,
-    width: float,
+def get_random_resized_crops(
+    height_small: float,
+    width_small: float,
+    height_large: float,
+    width_large: float,
+    n_small: int,
+    n_large: int,
     prob: float = 1.0,
 ):
-    def _random_resized_crop(image, p=1.0):
+    def _random_resized_crops(image, p=1.0):
         """Randomly crop and resize an image.
         Args:
             image: `Tensor` representing an image of arbitrary size.
-            height: Height of output image.
-            width: Width of output image.
+            height_small: Height of small crop.
+            width_small: Width of small crop.
+            height_large: Height of large crop.
+            width_large: Width of large crop.
+            n_small: Number of small crops.
+            n_large: Number of large crops.
             p: Probability of applying this transformation.
         Returns:
             A preprocessed image `Tensor`.
         """
         def _transform(image):  # pylint: disable=missing-docstring
-            image = _crop_and_resize(image, height, width)
-            return image
-        return random_apply(_transform, p=p, x=image)
+            images_small = [_crop_and_resize(image, height_small, width_small) for i in range(n_small)]
+            images_large = [_crop_and_resize(image, height_large, width_large) for i in range(n_large)]
+            images = images_small + images_large
+            # combine these images together in a way that can be returned...
+            # tf will not return a list
+            # can't stack because images have different sizes
+            return images
+        return _random_apply(_transform, p=p, x=image)
 
     def _crop_and_resize(image, height, width):
         """Make a random crop and resize it to height `height` and width `width`.
@@ -159,6 +159,66 @@ def get_random_resized_crop(
             max_attempts=100,
             scope=None,
         )
-        return tf.image.resize([image], [height, width], method=tf.image.ResizeMethod.BILINEAR)[0]
+        return tf.image.resize_bicubic([image], [int(height), int(width)])[0]
+
+    def _random_apply(func, p, x):
+        """Randomly apply function func to x with probability p."""
+        return tf.cond(
+            tf.less(tf.random.uniform([], minval=0, maxval=1, dtype=tf.float32),
+                tf.cast(p, tf.float32)),
+            lambda: func(x),
+            lambda: x)
 
     return _random_resized_crops
+
+def distorted_bounding_box_crop(
+    image,
+    bbox,
+    min_object_covered=0.1,
+    aspect_ratio_range=(0.75, 1.33),
+    area_range=(0.05, 1.0),
+    max_attempts=100,
+    scope=None,
+):
+    """Generates cropped_image using one of the bboxes randomly distorted.
+    See `tf.image.sample_distorted_bounding_box` for more documentation.
+    Borrowed from SIMCLR ground truth implementation.
+    Args:
+        image: `Tensor` of image data.
+        bbox: `Tensor` of bounding boxes arranged `[1, num_boxes, coords]`
+            where each coordinate is [0, 1) and the coordinates are arranged
+            as `[ymin, xmin, ymax, xmax]`. If num_boxes is 0 then use the whole
+            image.
+        min_object_covered: An optional `float`. Defaults to `0.1`. The cropped
+            area of the image must contain at least this fraction of any bounding
+            box supplied.
+        aspect_ratio_range: An optional list of `float`s. The cropped area of the
+            image must have an aspect ratio = width / height within this range.
+        area_range: An optional list of `float`s. The cropped area of the image
+            must contain a fraction of the supplied image within in this range.
+        max_attempts: An optional `int`. Number of attempts at generating a cropped
+            region of the image of the specified constraints. After `max_attempts`
+            failures, return the entire image.
+            scope: Optional `str` for name scope.
+    Returns:
+        (cropped image `Tensor`, distorted bbox `Tensor`).
+    """
+    with tf.name_scope(scope, 'distorted_bounding_box_crop', [image, bbox]):
+        shape = tf.shape(image)
+        sample_distorted_bounding_box = tf.image.sample_distorted_bounding_box(
+            shape,
+            bounding_boxes=bbox,
+            min_object_covered=min_object_covered,
+            aspect_ratio_range=aspect_ratio_range,
+            area_range=area_range,
+            max_attempts=max_attempts,
+            use_image_if_no_bounding_boxes=True)
+        bbox_begin, bbox_size, _ = sample_distorted_bounding_box
+
+        # Crop the image to the specified bounding box.
+        offset_y, offset_x, _ = tf.unstack(bbox_begin)
+        target_height, target_width, _ = tf.unstack(bbox_size)
+        image = tf.image.crop_to_bounding_box(
+            image, offset_y, offset_x, target_height, target_width)
+
+        return image
--- tests/data/dataloader_test.py
+++ tests/data/dataloader_test.py
@@ -1,13 +1,12 @@
 import jax.random
+import jax.numpy as jnp
 import numpy as np
 import pytest
 from omegaconf import DictConfig, OmegaConf
 from ssljax.data.dataloader import ScenicData, scenic
 
 
-"""
-# TODO: test dataloader arguments
-@pytest.mark.parametrize("batch_size", [10, 20])
+@pytest.mark.parametrize("batch_size", [16, 32])
 def test_dataloader(batch_size):
     rng = jax.random.PRNGKey(0)
     loader = scenic(
@@ -25,41 +24,85 @@ def test_dataloader(batch_size):
     assert isinstance(data["inputs"], np.ndarray)
 
 
-@pytest.mark.parametrize("batch_size", [10, 20])
+@pytest.mark.parametrize("batch_size", [16, 32])
 def test_bit(batch_size):
     rng = jax.random.PRNGKey(0)
     # test passthrough
     loader = scenic(
         OmegaConf.create(
             {
+                "experiment_name": "test",
                 "dataset_name": "bit",
-                "batch_size": batch_size,
                 "data_dtype_str": "float32",
-                "dataset_configs": {"pp_train": ("identity()"), "dataset": "imagenet2012", "num_classes": 1000, "train_split": "train", "val_split": "validation"},
+                "batch_size": batch_size,
+                "dataset_configs": {
+                    "pp_train": (
+                        "decode_jpeg_and_inception_crop(224)"
+                        "|flip_lr"
+                        "|randaug(2,15)"
+                        "|value_range(-1, 1)"
+                        "|onehot(1000, key='label', key_result='labels')"
+                        "|keep('image', 'labels')"
+                    ),
+                    "pp_eval": (
+                        "decode"
+                        "|resize_small(256)"
+                        "|central_crop(224)"
+                        "|value_range(-1, 1)"
+                        "|onehot(1000, key='label', key_result='labels')"
+                        "|keep('image', 'labels')"
+                    ),
+                    "dataset": "imagenet2012",
+                    "num_classes": 1000,
+                    "train_split": "train",
+                    "val_split": "validation",
+                    "prefetch_to_device": 2,
+                },
             }
         ),
         data_rng=rng,
     )
     data = next(iter(loader.train_iter))
-    print(data.shape)
-    assert isinstance(data["inputs"], np.ndarray)
+    assert isinstance(data["inputs"], jnp.ndarray)
 
-"""
-@pytest.mark.parametrize("batch_size", [10, 20])
-def test_random_resized_crop(batch_size):
+@pytest.mark.parametrize("batch_size", [16, 32])
+def test_random_resized_crops(batch_size):
     rng = jax.random.PRNGKey(0)
-    # test passthrough
+    # test rrc
     loader = scenic(
         OmegaConf.create(
             {
                 "dataset_name": "bit",
                 "batch_size": batch_size,
                 "data_dtype_str": "float32",
-                "dataset_configs": {"pp_train": ("random_resized_crop(height=224., width=224.)"), "dataset": "imagenet2012", "num_classes": 1000, "train_split": "train", "val_split": "validation"},
+                "dataset_configs": {
+                    "dataset": "imagenet2012",
+                    "num_classes": 1000,
+                    "train_split": "train",
+                    "val_split": "validation",
+                    "pp_train": (
+                        "decode"
+                        "|value_range(-1, 1)"
+                        "|onehot(1000, key='label', key_result='labels')"
+                        "|random_resized_crops(height_large=224., width_large=224., height_small=224., width_small=224., n_small=6, n_large=2)"
+                        "|keep('image', 'labels')"
+                    ),
+                    "pp_eval": (
+                        "decode"
+                        "|resize_small(256)"
+                        "|central_crop(224)"
+                        "|value_range(-1, 1)"
+                        "|onehot(1000, key='label', key_result='labels')"
+                        "|keep('image', 'labels')"
+                    ),
+                    "prefetch_to_device": 2,
+                    "shuffle_buffer_size": 250_000
+                },
             }
         ),
         data_rng=rng,
     )
     data = next(iter(loader.train_iter))
-    print(data.shape)
-    assert isinstance(data["inputs"], np.ndarray)
+    print(data["inputs"].shape)
+    assert isinstance(data["inputs"], jnp.ndarray)
+    assert False
