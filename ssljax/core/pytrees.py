28e7cd2c69ee186091668196601c40393e421ad0
pika <akaberto@umich.edu>
2022-02-02 21:55:55 -0500

    Add masking functions

--- ssljax/core/pytrees.py
+++ ssljax/core/pytrees.py
@@ -3,8 +3,8 @@ from flax import traverse_util
 from flax.traverse_util import (Traversal, _EmptyNode, _get_params_dict,
                                 _sorted_items, empty_node, flatten_dict,
                                 unflatten_dict)
-
-
+import fnmatch
+from typing import  AnyStr, List
 def add_prefix_to_dict_keys(d, prefix="branches_"):
     return {prefix + str(k): v for k, v in d.items()}
 
@@ -71,3 +71,76 @@ class ModelParamFilter(Traversal):
             return flax.core.FrozenDict(new_params)
         else:
             return new_params
+
+
+
+def inc_filter_path(path: AnyStr, patterns: List[AnyStr]) -> bool:
+    """
+    Returns True if the given path string matches any in the pattern
+    Args:
+      path (Str): String.  This is assumed to be a path (e.g., conv_1/bias)
+      patterns (List[Str]): List of patterns against which the path is compared
+
+
+    Example::
+
+      path = "a/b/c"
+      pats = ["*/c"]
+      inc_filter_path(path, pats)
+      >> True
+    """
+    for pat in patterns:
+        if len(fnmatch.filter([path], pat)) != 0:
+               return True
+    else:
+        return False
+
+def exc_filter_path(path: AnyStr, patterns: List[AnyStr]) -> bool:
+    """
+    Returns False if the given path string matches any in the pattern
+    Args:
+      path (Str): This is assumed to be a path (e.g., conv_1/bias)
+      patterns (List[Str]): List of patterns against which the path is compared
+
+
+    Example::
+
+      path = "a/b/c"
+      pats = ["*/c"]
+      inc_filter_path(path, pats)
+      >> True
+    """
+    return not inc_filter_path(path, patterns)
+
+def inc_exc_filter(path, inc_filters, exc_filters):
+    """
+    If the path is covered by the inc_filters, it then checks if it is in the
+    exclude list. If it is covered by the exc_filters, then the function
+    returns False. Else, it returns a True. If it is not present in
+    inc_filters, it returns a False
+
+    Args:
+      path (Str): This is assumed to be a path like conv_1/bias.
+      inc_filters (List[Str]): List of inclusionary patterns
+      exc_filters (List[Str]): List of exclusionary patterns
+    """
+    if inc_filter_path(path, inc_filters):
+        if exc_filters is not None:
+            return exc_filter_path(path, exc_filters)
+        else:
+            return True
+    else:
+        return False
+
+
+def flattened_traversal_with_filter(fn):
+  def mask(data):
+    flat = traverse_util.flatten_dict(data)
+    ret_dict = {}
+    for k, v in flat.items():
+        if fn("/".join(k)):
+            ret_dict[k] =  True
+        else:
+            ret_dict[k] =  False
+    return traverse_util.unflatten_dict(ret_dict)
+  return mask
